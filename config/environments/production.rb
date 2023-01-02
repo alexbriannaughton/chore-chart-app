4fc9b15ad446e01b57b73cac417ca8091b8825dd
Alex Brian Naughton <alexbriannaughton@gmail.com>
2023-01-01 20:10:26 -0800

    email test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
   ActionMailer::Base.smtp_settings = {
     :user_name => 'apikey', # This is the string literal 'apikey', NOT the ID of your API key
     :password => Rails.application.credentials.sendgrid_api_key, # This is the secret sendgrid API key which was issued during API key creation
-    :domain => 'yourdomain.com',
+    :domain => 'https://chore-chart-app.onrender.com/',
     :address => 'smtp.sendgrid.net',
     :port => 587,
     :authentication => :plain,
