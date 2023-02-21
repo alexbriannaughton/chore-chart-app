46df5d1521a8117191f431a2b7ae5cfecf48c40a
Alex Brian Naughton <alexbriannaughton@gmail.com>
2023-02-20 19:52:32 -0800

    more added annotations

--- app/models/chore_wheel.rb
+++ app/models/chore_wheel.rb
@@ -9,19 +9,24 @@ class ChoreWheel < ApplicationRecord
     validates :name, length: { minimum: 2 }
     validates :auto_rotate, inclusion: { in: [ true, false ] }
 
+
+    # rotates tasks on chore wheel one member over
     def rotate
+        # grab number for every member in the chore wheel
         num = self.members.length
 
-        arr = []
-
+        # make array of ids for all the chore wheel's current tasks
+        tasks_arr = []
         self.member_tasks.last(num).each do |mt|
-            arr << mt.task.id
+            tasks_arr << mt.task.id
         end
 
-        self.members.each_with_index do |i, index|
-            MemberTask.create!(chore_wheel: self, member_id: i.id, task_id: arr.rotate(-1)[index])
+        # create new member_task for each member with the tasks array rotated
+        self.members.each_with_index do |mem, index|
+            MemberTask.create!(chore_wheel: self, member_id: mem.id, task_id: tasks_arr.rotate(-1)[index])
         end
 
+        # email new tasks to the members
         self.member_tasks.last(num).each do |mt|
             mt.send_new_task_email
         end
--- app/models/member_task.rb
+++ app/models/member_task.rb
@@ -48,4 +48,5 @@ class MemberTask < ApplicationRecord
 
   end
 
+  
 end
